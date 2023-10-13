85e67bca365377916b8d19712ef94ad76c934cdb
Santiago Blachere <gotregotregotre@gmail.com>
2023-10-13 17:23:18 -0300

    feat: renderizar todos segun proyecto clickeado

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,7 @@
 
 export const domManipulation = () => {
     /* global variables */
-    const projectsArray = [{name:'any'}];
+    const projectsArray = [];
     const toDos = [];
     let toDosUpdated = [];
     /* classes */
@@ -12,7 +12,8 @@ export const domManipulation = () => {
             this.dueDate = dueDate,
             this.priority = priority,
             this.notes = notes,
-            this.project = 'any'
+            this.project = 'any';
+            this.any = 'any'
             toDos.push( { ...this } )
         }
 
@@ -35,101 +36,137 @@ export const domManipulation = () => {
     
     const projectardo = new Project('proyecto 1');
     const projectardo2 = new Project('proyecto 2');
-    renderToDo(projectsArray, toDos);
-   
     renderProjects(projectsArray, toDos);
+    renderToDo(projectsArray, toDos);
+    
+   
+    
     
     /* render a todo item */
     function renderToDo (projects, toDos) {
     const root = document.querySelector('#root');
-    
-    const todoContainer = document.createElement('div');
-    todoContainer.classList.add('toDoCard')
-    
-
-    const title = document.createElement('h2');
-    title.innerText = `${todo.title}`
-    todoContainer.appendChild(title);
-
-    const description = document.createElement('p');
-    description.innerText = `description: ${todo.description}`;
-    todoContainer.appendChild(description);
-
-    const dueDate = document.createElement('p');
-    dueDate.innerText = `Due date: ${todo.dueDate}`;
-    todoContainer.appendChild(dueDate);
-
-    const priority = document.createElement('p');
-    priority.innerText = `Priority: ${todo.priority}`;
-    todoContainer.appendChild(priority);
-
-    const notes = document.createElement('p');
-    notes.innerText = `notes: ${todo.notes}`;
-    todoContainer.appendChild(notes);
-
-    const projectForm = document.createElement('form');
-    
-
-    const selectLabel = document.createElement('label');
-    selectLabel.setAttribute('for', 'projects');
-    selectLabel.innerText = 'choose project: '
-    
-    const projectSelect = document.createElement('select');
-    projectSelect.setAttribute('name', 'projects');
-    projectSelect.setAttribute('id', 'projects');
-    projectSelect.classList.add('select')
-    
-    projects.forEach(project => {
+   
         
-        const option = document.createElement('option');
-        option.classList.add(`option`);
-        option.setAttribute('value', `${project.name}`);
-        option.innerText = `${project.name}`
+    const toDosPrevious = document.querySelector('.toDoSection');
+    if (toDosPrevious) {
+        toDosPrevious.remove()
         
-        
-        projectSelect.appendChild(option);
-    });
+    }
     
-    projectForm.onchange = handleChange;
-    projectForm.appendChild(selectLabel);
-    projectForm.appendChild(projectSelect);
     
-
-    todoContainer.appendChild(projectForm);
-    root.appendChild(todoContainer);
+    const toDoSection = document.createElement('section');
+    toDoSection.classList.add('toDoSection');
+    root.appendChild(toDoSection)
+    toDos.forEach((todo) => {
+        
     
-    function handleChange (e) {
-        e.preventDefault();
-       
-        const selectedProject = e.target.value;
+        const todoContainer = document.createElement('div');
+        todoContainer.classList.add('toDoCard')
         
-        todo.project = selectedProject;
+    
+        const title = document.createElement('h2');
+        title.innerText = `${todo.title}`
+        todoContainer.appendChild(title);
+    
+        const description = document.createElement('p');
+        description.innerText = `description: ${todo.description}`;
+        todoContainer.appendChild(description);
+    
+        const dueDate = document.createElement('p');
+        dueDate.innerText = `Due date: ${todo.dueDate}`;
+        todoContainer.appendChild(dueDate);
+    
+        const priority = document.createElement('p');
+        priority.innerText = `Priority: ${todo.priority}`;
+        todoContainer.appendChild(priority);
+    
+        const notes = document.createElement('p');
+        notes.innerText = `notes: ${todo.notes}`;
+        todoContainer.appendChild(notes);
+    
+        const projectForm = document.createElement('form');
         
-        const inArray = toDos.findIndex((el) => {
+    
+        const selectLabel = document.createElement('label');
+        selectLabel.setAttribute('for', 'projects');
+        selectLabel.innerText = 'choose project: '
+        
+        const projectSelect = document.createElement('select');
+        projectSelect.setAttribute('name', 'projects');
+        projectSelect.setAttribute('id', 'projects');
+        projectSelect.classList.add('select')
+        const anyOption = document.createElement('option');
+        anyOption.classList.add('option');
+        anyOption.setAttribute('value', 'any');
+        anyOption.innerText = 'Any';
+        projectSelect.appendChild(anyOption)
+        projects.forEach(project => {
+        
+            const option = document.createElement('option');
+            option.classList.add(`option`);
+            option.setAttribute('value', `${project.name}`);
+            option.innerText = `${project.name}`
             
-            return el.title === todo.title
-        })
+            
+            projectSelect.appendChild(option);
+        });
+        projectForm.onchange = handleChange;
+        projectForm.appendChild(selectLabel);
+        projectForm.appendChild(projectSelect);
+        todoContainer.appendChild(projectForm);
         
-        toDos[inArray] = todo;
+
         
-        toDosUpdated = toDos;
-        
-        renderProjects(projectsArray, toDosUpdated)
-        
-        
-    }
-    }
+        function handleChange (e) {
+            e.preventDefault();
+           
+            const selectedProject = e.target.value;
+            
+            todo.project = selectedProject;
+            
+            const inArray = toDos.findIndex((el) => {
+                
+                return el.title === todo.title
+            })
+            
+            toDos[inArray] = todo;
+            
+            toDosUpdated = toDos;
+            
+            renderProjects(projects, toDosUpdated)
+            
+            
+            
+        }
+        toDoSection.appendChild(todoContainer);
+    })
+     
+}
     /* project rendering */
     function renderProjects(projects, toDos) {
         
-        
+        /* check if projects already exists, and remove it*/
         const previousContainer = document.querySelector('.projectsContainer')
         if(previousContainer) {
             previousContainer.remove();
         }
-        
+        /* if it doesnt exist, create it */
         const projectsContainer = document.createElement('div');
         projectsContainer.classList.add('projectsContainer');
+        /* render any button */
+        const anyBtn = document.createElement('button');
+        anyBtn.innerText = 'Any';
+        anyBtn.classList.add('projectBtn');
+        anyBtn.setAttribute('any', 'any')
+        anyBtn.addEventListener('click', () => {
+            let projectToDos = toDos.filter( (el) => {
+                return el.any === 'any'
+            })
+            
+            renderToDo(projectsArray, projectToDos)
+        })
+        projectsContainer.appendChild(anyBtn)
+        /* render each project as a button, if there's more than 0*/
         if (projects.length > 0) {
             projects.forEach(project => {
             
@@ -144,14 +181,15 @@ export const domManipulation = () => {
         
                 projectsContainer.appendChild(projectBtn)
             });
-            
+            /* event listener, renders every element that corresponds with the project clicked*/
             function handleClick(e){
             e.preventDefault
             let projectClicked = e.target.getAttribute('project-name');
+            
             let projectToDos = toDos.filter( (el) => {
-                return el.project === projectClicked
+                return el.project === projectClicked 
             })
-            renderToDo()
+            renderToDo(projectsArray, projectToDos)
             }
             
         }
